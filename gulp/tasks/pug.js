d6d96cede511ce7ce71d46bf8cbb230b63e045d0
AndrewDisin <andrewdisin@gmail.com>
2017-12-04 14:08:40 +0200

    Refactoring json files structure

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -5,6 +5,12 @@ module.exports = function() {
                 locals : {
                     nav: JSON.parse($.fs.readFileSync('./data/navigation.json', 'utf8')),
                     content: JSON.parse($.fs.readFileSync('./data/content.json', 'utf8')),
+                    contentCatalog: JSON.parse($.fs.readFileSync('./data/contentCatalog.json', 'utf8')),
+                    contentPackagin: JSON.parse($.fs.readFileSync('./data/contentPackagin.json', 'utf8')),
+                    contentNewYear: JSON.parse($.fs.readFileSync('./data/contentNewYear.json', 'utf8')),
+                    contentRoom: JSON.parse($.fs.readFileSync('./data/contentRoom.json', 'utf8')),
+                    contentDelivery: JSON.parse($.fs.readFileSync('./data/contentDelivery.json', 'utf8')),
+                    contentBlog: JSON.parse($.fs.readFileSync('./data/contentBlog.json', 'utf8')),
                 },
                 pretty: true
             }))
